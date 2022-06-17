e355d90b473534fe117cea56beae4134f0b9f93b
neptuniam <ljones1997@hotmail.com>
2022-06-17 08:45:16 -0600

    Add back sentText listener

--- src/views/ChatRoom.vue
+++ src/views/ChatRoom.vue
@@ -237,7 +237,7 @@
       </div>
 
 
-      <message-action-bar @setRoomEncryptStatus="setRoomEncryptStatus" />
+      <message-action-bar @setRoomEncryptStatus="setRoomEncryptStatus" @sendText="sendText" />
 
       <v-row id="inputRow">
         <v-textarea
